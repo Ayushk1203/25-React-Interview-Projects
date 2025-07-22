be958b8e1e0c57a85500b52d23bfa8de855814e2
Ayushk1203 <ayushkhare12.03.2003@gmail.com>
2025-07-22 10:51:50 +0530

    Completed Interview Question Mini Project 2 - Random Color Generator

--- React 25 Interview Projects Combined/Interview Projects/src/App.jsx
+++ React 25 Interview Projects Combined/Interview Projects/src/App.jsx
@@ -1,15 +1,17 @@
 
 import './App.css'
 import Accordian from './components/accordian'
+import RandomColor from './components/random-color'
 
 function App() {
   
 
   return (
     <>
-      <Accordian/>
+      {/*<Accordian/>*/}
+      <RandomColor/>
     </>
   )
 }
 
-export default App
+export default App;
